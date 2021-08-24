ae2ce08bc6c28d1112046daffdb2f090b1951b8c
fridacasg <A01742173@itesm.mx>
2021-08-24 03:19:49 +0000

    Tareas04

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,6 +1,19 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    peso = float(input("Peso en kg: ")) 
+    altura = float(input("Altura en m: "))
+    indice = float(int( peso / altura**2)) 
+    if indice < 20:
+        print("PESO BAJO") 
+    elif 20 <= indice < 25:
+            print("NORMAL")
+    elif 25 <= indice < 30:
+        print("SOBREPESO")	
+    elif 30 <= indice < 40:
+        print("OBESIDAD")
+    elif indice >= 40:
+        print("OBESIDAD MORBIDA")
+    
 
 if __name__=='__main__':
     main()
